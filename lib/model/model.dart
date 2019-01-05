2b2770709f6a8bea8e650a05724fceb9fe8f4076
guanox <kajprintz@gmail.com>
2019-01-05 02:36:26 +0100

    removes old tests

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:async';
 
 class Startup {
   String key;
@@ -13,23 +13,20 @@ class Startup {
 
   Startup._internal(this.key, this.name);
 
-  Startup copyWith({String key, String name }) {
+  Startup copyWith({String key, String name}) {
     return Startup._internal(
-        key ?? this.key,
-        name ?? this.name,
-      );
+      key ?? this.key,
+      name ?? this.name,
+    );
   }
 
-  Startup.fromSnapshot(DataSnapshot snapshot) 
-    : key = snapshot.key,
-      name = snapshot.value['name'];
+  Startup.fromSnapshot(DataSnapshot snapshot)
+      : key = snapshot.key,
+        name = snapshot.value['name'];
 
-  Map toJson() => {
-    'key': key,
-    'name': name
-  };
+  Map toJson() => {'key': key, 'name': name};
 
-  @override 
+  @override
   String toString() {
     return toJson().toString();
   }
@@ -44,13 +41,11 @@ class AppState {
     @required this.firebaseState,
   });
 
-  AppState.initialState()  
-    : startups = List.unmodifiable(<Startup>[]),
-    firebaseState = new FirebaseState();
+  AppState.initialState()
+      : startups = List.unmodifiable(<Startup>[]),
+        firebaseState = new FirebaseState();
 
-  Map toJson() => {
-    'startups': startups
-  };
+  Map toJson() => {'startups': startups};
 
   @override
   String toString() {
@@ -64,14 +59,22 @@ class FirebaseState {
   final StreamSubscription<Event> subRemoveStartup;
   final FirebaseUser user;
 
-  const FirebaseState({this.mainReference, this.user, this.subAddStartup, this.subRemoveStartup});
+  const FirebaseState(
+      {this.mainReference,
+      this.user,
+      this.subAddStartup,
+      this.subRemoveStartup});
 
-  FirebaseState copyWith({ DatabaseReference mainReference, FirebaseUser user, StreamSubscription<Event> subAddStartup, StreamSubscription<Event> subRemoveStartup}) {
+  FirebaseState copyWith(
+      {DatabaseReference mainReference,
+      FirebaseUser user,
+      StreamSubscription<Event> subAddStartup,
+      StreamSubscription<Event> subRemoveStartup}) {
     return new FirebaseState(
-        mainReference: mainReference ?? this.mainReference,
-        user: user ?? this.user,
-        subAddStartup: subAddStartup ?? this.subAddStartup,
-        subRemoveStartup: subRemoveStartup ?? this.subRemoveStartup,
-        );
+      mainReference: mainReference ?? this.mainReference,
+      user: user ?? this.user,
+      subAddStartup: subAddStartup ?? this.subAddStartup,
+      subRemoveStartup: subRemoveStartup ?? this.subRemoveStartup,
+    );
   }
 }
